9c9e0452b8319a7ab2d7d26b54e0595d8f03d865
SamHez <samuelepodoi@gmail.com>
2026-02-11 17:23:13 +0200

    fix: whitelist via.placeholder.com in next.config.ts

--- filmflux/next.config.ts
+++ filmflux/next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/t/p/**",
       },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
