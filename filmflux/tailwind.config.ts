fb0cfe5a09c93e4cec6385be0caeaee5960a9361
SamHez <samuelepodoi@gmail.com>
2026-02-11 14:49:18 +0200

    chore: clean default Next.js boilerplate and reset styles

--- filmflux/tailwind.config.ts
+++ filmflux/tailwind.config.ts
@@ -1,10 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-export default {
-  content: [],
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./services/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config
-
+};
+export default config;
