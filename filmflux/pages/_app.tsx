ce722cd1fe54eee126241cbe21f1e91f5d92365b
SamHez <samuelepodoi@gmail.com>
2026-02-12 18:07:06 +0200

    feat: add custom FF favicon with blue and gray branding

--- filmflux/pages/_app.tsx
+++ filmflux/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import { WatchlistProvider } from "@/contexts/WatchlistContext";
@@ -10,6 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
+      <Head>
+        <title>FilmFlux</title>
+        <meta name="description" content="Modern Movie exploration platform" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <ThemeProvider>
         <WatchlistProvider>
           <Layout>
